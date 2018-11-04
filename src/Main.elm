bc2cb73df1812762a2a975f4af174b66f1bd68ab
Nik Silver <nik@niksilver.com>
2018-11-04 16:09:54 +0000

    A slightly more complex model.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,7 @@
 import Browser
-import Html exposing (Html, text)
+import Html exposing (Html, div, span, text)
+import Html.Attributes exposing (style)
+
 
 main =
   Browser.sandbox
@@ -9,11 +11,15 @@ main =
   }
 
 
-type alias Model = ()
+type alias Model =
+  { left : Bool
+  , right : Bool
+  }
+
 
 init : Model
 init =
-  ()
+  Model False True
 
 
 type alias Msg = ()
@@ -21,13 +27,28 @@ type alias Msg = ()
 
 update : Msg -> Model -> Model
 update msg model =
-  ()
+  model
 
 
 view : Model -> Html Msg
 view model =
-  text "Hello, world!"
+  div [ style "padding" "10px" ]
+    [ viewContainer model.left
+    , viewContainer model.right
+    ]
 
+
+viewContainer : Bool -> Html Msg
+viewContainer show =
+  case show of
+    True ->
+      span [ style "padding" "10px" ]
+        [ text "Here I am!"
+        ]
+    False ->
+      span [ style "padding" "10px" ]
+        [ text "Nothing to see"
+        ]
 
 
 
